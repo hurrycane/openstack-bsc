a7cc6e557669c7b5d03e88c54650597abba4fc65
Bogdan Gaza <bogdan.gaza@yahoo.com>
2011-12-30 12:20:54 +0000

    Added misc tools. First tool is password setter

---
+++ modules/tools/manifests/init.pp
@@ -0,0 +1,3 @@
+class tools {
+  include tools::install
+}
---
+++ modules/tools/manifests/install.pp
@@ -0,0 +1,25 @@
+class tools::install {
+  file { '/opt' :
+    ensure      => directory,
+    owner       => 'root',
+    group       => 'root'
+    mode        => 755
+  }
+
+  file { '/opt/tools' :
+    ensure      => directory,
+    owner       => 'root',
+    group       => 'root'
+    mode        => 755,
+    require     => File["/opt"]
+  }
+
+  file { '/opt/tools/setuserpassword' :
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 0511,
+    source => "puppet:///tools/setuserpassword"
+  }
+
+}
